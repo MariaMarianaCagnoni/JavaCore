1fdf4a1e125f273831cb374c143176ca4c050836
mariana <mariamariana.cagnoni@gmail.com>
2023-08-10 15:38:03 -0300

    LocalDate terminado

--- src/main/java/Rdates/test/TIME/LocalDateTest01.java
+++ src/main/java/Rdates/test/TIME/LocalDateTest01.java
@@ -1,6 +1,5 @@
 package Rdates.test.TIME;
 
-import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.Calendar;
@@ -18,14 +17,21 @@ public class LocalDateTest01 {
         System.out.println(Calendar.getInstance());
         LocalDate ld = LocalDate.of(2002,12,22);
         LocalDate ld2 = LocalDate.of(2023, Month.JULY, 30);
-        System.out.println(ld);
+
+        //LocalDate ld3 = new LocalDate();
+        LocalDate ld3 = LocalDate.now();
+        System.out.println("data de  agora: "+ ld3);
+
+        /*
+         * Contrutor é privado, portando não se pode utilizar o 'new'
+         */
+        System.out.println(ld);//2001-12-22 geralmente é assim que é salvo no BD
         System.out.println(ld.getEra());
         System.out.println(ld.getChronology());
         System.out.println(ld.getDayOfWeek());
         System.out.println(ld.getMonth());
         System.out.println(ld.minusMonths(12)); //subtrai os meses passados
-
-
         System.out.println(ld2.isLeapYear());
+        System.out.println(ld2.getYear());
     }
 }

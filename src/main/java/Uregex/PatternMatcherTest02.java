ee9f0879c9eb4f29bbb3718a46026a36944b51ae
mariana <mariamariana.cagnoni@gmail.com>
2023-08-15 16:14:09 -0300

    regex quantificadores

--- src/main/java/Uregex/PatternMatcherTest02.java
+++ src/main/java/Uregex/PatternMatcherTest02.java
@@ -13,22 +13,31 @@ public class PatternMatcherTest02 {
         // \D = tudo que nao for digito
         // \s = todos os espaços em branco  \t \f \n \r
         // \S = todos os caracteres excluindo os brancos
-        // \w = tudo de a-zA-Z,digitos,_
+        // \w = tudo de a-zA-Z,dígitos,_
         // \W = tudo que não for o \w
-
+        // [] caracter de RANGE
 
 
         //encontra padrẽs no texto através de meta-characteres
-        String regex = "\\D";
+        String regex2 = "[ABCabc]";
+        //exemplo2: [a-zA-C] retorna tudo de a a z e de A a C
         String regex1 = "\\s";
         String texto = "jdsbfv6g4\t582r hbvffbjwkr";
+        String texto2 = "ABRACOabra";
 
         //precisam ser compiladas
-        Pattern pattern = Pattern.compile(regex1);
-        Matcher matcher = pattern.matcher(texto);
+        Pattern pattern = Pattern.compile(regex2);
+        Matcher matcher = pattern.matcher(texto2);
 
-        while(matcher.find()){
-            System.out.print(matcher.start()+ " ");
+        while (matcher.find()) {
+            System.out.print(matcher.start() + " ");
         }
+        int hexaDecimal = 0x69F86A;
+        //números exadecimais no java começam com 0x sempre
+
+        System.out.println("\n"+ hexaDecimal);
+
+
     }
+
 }
---
+++ src/main/java/Uregex/PatternMatcherTest03.java
@@ -0,0 +1,30 @@
+package Uregex;
+
+/**
+ * @author mariana
+ * @Project: javacore
+ */
+public class PatternMatcherTest03 {
+    public static void main(String[] args) {
+        // \d = todos os digitos
+        // \D = tudo que nao for digito
+        // \s = todos os espaços em branco  \t \f \n \r
+        // \S = todos os caracteres excluindo os brancos
+        // \w = tudo de a-zA-Z,dígitos,_
+        // \W = tudo que não for o \w
+        // [] caracter de RANGE
+        // ? = zero ou uma
+        //* = zero ou mais
+        // +  = uma ou mais ocorrencias
+        //{n,m} = de n até m numero de ocorrencias
+        // () = agrupamento
+        // | = 'ou'
+        // $ = fim da linha
+
+
+        String textoHexa = "h 567 jh 0x1 0x1652FA";
+        String regex4 = "0[xX]([0-9a-fA-F])+(\\s|$)";
+
+    }
+
+}

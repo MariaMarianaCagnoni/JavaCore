eac20a175e338b97a5cba47432dc9a5c4ca30268
mariana <mariamariana.cagnoni@gmail.com>
2023-08-16 12:59:54 -0300

    BuferedReader terminado

---
+++ src/main/java/Vio/test/BufferedReaderTest01.java
@@ -0,0 +1,30 @@
+package Vio.test;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+/**
+ * @author mariana
+ * @Project: javacore
+ */
+public class BufferedReaderTest01 {
+    public static void main(String[] args) {
+
+        File file = new File("file2.txt");
+        boolean exists = file.exists();
+        System.out.println(exists);
+
+        try (FileReader fr = new FileReader(file);
+             BufferedReader br = new BufferedReader(fr)) {
+            String linha;
+            while ((linha = br.readLine()) != null) {
+                System.out.println(linha);
+
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}

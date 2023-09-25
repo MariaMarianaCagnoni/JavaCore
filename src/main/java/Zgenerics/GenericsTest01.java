8a327011980e589e041ac99178b17bb735184af9
mariana <mariamariana.cagnoni@gmail.com>
2023-09-25 02:07:56 -0300

    generics introducao

---
+++ src/main/java/Zgenerics/GenericsTest01.java
@@ -0,0 +1,44 @@
+package Zgenerics;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author mariana
+ * @Project: javacore
+ */
+public class GenericsTest01 {
+    public static void main(String[] args) {
+
+        //antes do Generics
+        List lista = new ArrayList();
+
+        lista.add("mariana");
+        lista.add(34456);
+        lista.add(true);
+        lista.add('Q');
+
+        for (Object list : lista) {
+            if(list instanceof String){
+                System.out.println(lista);
+            }
+            if(list instanceof Double){
+                System.out.println(lista);
+            }
+            if (list instanceof Boolean){
+                System.out.println(lista);
+            }
+            if (list instanceof Character){
+                System.out.println(lista);
+            }
+        }
+        //Com generics
+        List<String> lista2 = new ArrayList<>();
+        lista2.add("beeru");
+        lista2.add("Whiz");
+
+        //verifica em tempo de compilação
+        //type erasure (apaga o tipo depois que o código é compilado
+
+    }
+}

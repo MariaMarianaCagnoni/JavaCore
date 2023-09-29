4c1618c05404a0e4c56ba056574350052648b17b
mariana <mariamariana.cagnoni@gmail.com>
2023-09-29 14:27:40 -0300

    wildcard part02

--- src/main/java/Zgenerics/WildCardTest02.java
+++ src/main/java/Zgenerics/WildCardTest02.java
@@ -1,5 +1,6 @@
 package Zgenerics;
 
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -10,17 +11,31 @@ import java.util.List;
 
 public class WildCardTest02 {
     public static void main(String[] args) {
-        List<Cachorro> cachorros = List.of(new Cachorro(),new Cachorro());
-        List<Gato> gatos = List.of(new Gato(),new Gato());
+        List<Cachorro> cachorros = List.of(new Cachorro(), new Cachorro());
+        List<Gato> gatos = List.of(new Gato(), new Gato());
         //printConsulta(gatos); erro de compilação,pois o java não save o tipo de lista voce esta passando
     }
 
-    private static void printConsulta(List<Animal> animal){
+    //Type erasure ?
+    private static void printConsulta(List<? extends Animal> animal) {
+        //aceita qualquer tipo de lista que extends(filho) Animal
         for (Animal animal1 : animal) {
             animal1.consulta();
         }
-        //animal[1] = new Gato(); tentando colocar uma gato em uma posicao de cachorro
+        //animal[1] = new Gato(); tentando colocar um gato em uma posição de cachorro
 
     }
 
+    private static void printConsulta2(List<? super Animal> animals) {
+        //aceita qualquer tipo de lista que extends(pai) Animal
+        //só irá aceita SUPER classes excluindo as subclasses(cachorro e gato)
+       Animal animal01 = new Cachorro();
+       Animal animal02 = new Gato();
+       animals.add(animal02);
+       animals.add(animal01);
+
+    }
+
+    //quando usar o wildcard: não pode add novos elementos na lista
+    //independente de ser uma classe ou interface a palavra sempre é extends: List<? extends Animal>
 }

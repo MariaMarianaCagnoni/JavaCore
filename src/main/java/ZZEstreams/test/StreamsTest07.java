fbaca96a3bb67134afea37398aefa13e4a34425d
mariana <mariamariana.cagnoni@gmail.com>
2023-11-06 16:30:47 -0300

    reduce part1

---
+++ src/main/java/ZZEstreams/test/StreamsTest07.java
@@ -0,0 +1,31 @@
+package ZZEstreams.test;
+
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * @author mariana
+ * @Project: javacore
+ */
+public class StreamsTest07 {
+    public static void main(String[] args) {
+        List<Integer> integers = List.of(1, 2, 3, 3, 4, 5);
+        integers.stream()
+                .reduce((x, y) -> x + y)
+                .ifPresent(System.out::println);
+        System.out.println("-----------------------------");
+
+        System.out.println(integers.stream()
+                .reduce(3, (x, y) -> x + y));
+
+        System.out.println(integers.stream()
+                .reduce(3, Integer::sum)); //mesma coisa que a funcao de cima.
+
+        System.out.println(integers.stream()
+                .reduce((x, y) -> x * y)
+                .orElseThrow());
+
+
+    }
+
+}
